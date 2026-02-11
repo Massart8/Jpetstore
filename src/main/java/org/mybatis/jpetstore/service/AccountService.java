f3298738132ec5d89b3ab47e1d541ead9cf16fe0
Kya03 <160619731+Kya03@users.noreply.github.com>
2026-02-11 20:01:06 +0000

    Update AccountService.java
    Clarté du code : Utiliser !password.isEmpty() est plus explicite que de vérifier si la longueur est supérieure à zéro. C'est la convention standard en Java moderne.

--- src/main/java/org/mybatis/jpetstore/service/AccountService.java
+++ src/main/java/org/mybatis/jpetstore/service/AccountService.java
@@ -1,17 +1,17 @@
 /*
- *    Copyright 2010-2022 the original author or authors.
+ * Copyright 2010-2022 the original author or authors.
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *       https://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.mybatis.jpetstore.service;
 
@@ -48,7 +48,7 @@ public class AccountService {
    * Insert account.
    *
    * @param account
-   *          the account
+   * the account
    */
   @Transactional
   public void insertAccount(Account account) {
@@ -61,14 +61,16 @@ public class AccountService {
    * Update account.
    *
    * @param account
-   *          the account
+   * the account
    */
   @Transactional
   public void updateAccount(Account account) {
     accountMapper.updateAccount(account);
     accountMapper.updateProfile(account);
 
-    Optional.ofNullable(account.getPassword()).filter(password -> password.length() > 0)
+    // CORRECTION : Remplacement de password.length() > 0 par !password.isEmpty()
+    Optional.ofNullable(account.getPassword())
+        .filter(password -> !password.isEmpty())
         .ifPresent(password -> accountMapper.updateSignon(account));
   }
 
